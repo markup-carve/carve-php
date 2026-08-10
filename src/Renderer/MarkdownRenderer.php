eee6933d2f4b02e2d0048d235dac64d11824b574
Mark Scherer <dereuromark@users.noreply.github.com>
2026-08-11 00:09:18 +0200

    Keep bidi stripping off the ASCII fast path

--- src/Renderer/MarkdownRenderer.php
+++ src/Renderer/MarkdownRenderer.php
@@ -1575,6 +1575,10 @@ class MarkdownRenderer implements RendererInterface
      */
     protected function stripControls(string $text): string
     {
-        return (string)preg_replace('/[\x{000D}\x{007F}-\x{009F}\x{202A}-\x{202E}\x{2066}-\x{2069}]/u', '', $text);
+        $text = (string)preg_replace('/[\x{000D}\x{007F}-\x{009F}]/u', '', $text);
+
+        return str_contains($text, "\xE2")
+            ? (string)preg_replace('/[\x{202A}-\x{202E}\x{2066}-\x{2069}]/u', '', $text)
+            : $text;
     }
 }
